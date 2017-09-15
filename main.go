b76c6c01009b02c5ddc034d93ddf6785a76d9090
Lili Cosic <lili@kinvolk.io>
2017-09-15 17:09:22 +0200

    *: add habitat specific files

--- main.go
+++ main.go
@@ -7,16 +7,15 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	f, err := ioutil.ReadFile("service.conf")
+	f, err := ioutil.ReadFile("../config/service.conf")
 	if err != nil {
 		panic(err)
 	}
 	msg := string(f)
-
 	io.WriteString(w, msg)
 }
 
 func main() {
 	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":5555", nil)
 }
